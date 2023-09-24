ccff028ae0e13266a3c8425e3d480cb84be6d9d5
angelamoore86 <122281605+angelamoore86@users.noreply.github.com>
2023-09-24 16:39:17 -0230

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import {RecipeList} from './Recipe';
+import AddRecipe from './form';
+import {Link} from "react-router-dom";
 
-function App() {
+function Home(){
+  const [recipes, setRecipes] = useState(null);
+
+  useEffect( () => {
+    fetch("./recipe.json")
+    .then( response => response.json() )
+    .then(setRecipes)
+    .catch(e => console.log(e.message));
+  }, [])
+
+  if (recipes == null) return;
+  
+  const addRecipe = (newRecipe) => {
+    setRecipes([...recipes, newRecipe]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <Link to="/add">Add Recipe</Link>
+      </nav>
+      <h1>Recipes</h1>
+      <RecipeList recipesData={recipes} setRecipesData={setRecipes} />
+      <AddRecipe handleSubmit={addRecipe}/>
     </div>
   );
+}
+
+export function Add(){
+  const [recipes, setRecipes] = useState([]);
+
+  const addRecipe = (newRecipe) => {
+    setRecipes([...recipes, newRecipe]);
+  };
+  
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+      <h1>Add Recipe</h1>
+      <h3>To add a recipe fill out the form below.</h3>
+      <AddRecipe handleSubmit={addRecipe} />
+    </div>
+  ); 
+}
+
+export function App() {
+  return <Home />; 
 }
 
 export default App;
---
+++ src/form.js
@@ -0,0 +1,57 @@
+import {useState} from 'react';
+
+export function AddRecipe( { handleSubmit }) {
+
+const [recipe, setRecipe] = useState({
+    name: "",
+    ingredients: "",
+    directions: "",
+    description: "",
+    image: ""
+  });
+  const submitRecipe = (e) => {
+    e.preventDefault();
+    handleSubmit(recipe);
+    setRecipe({
+        id: "",
+        name: "",
+        ingredients: "",
+        directions: "",
+        description: "",
+        image: "",
+    });
+  };
+    return (
+        <div>
+            <form onSubmit={submitRecipe}>
+                <div>
+                    <label>Recipe Name: </label>
+                    <input type="text" value={recipe.name}
+                    onChange={ (e) => setRecipe({...recipe, name: e.target.value })}></input>
+                </div>
+                <div>
+                    <label>Ingredients: </label>
+                    <textarea type="text" value={recipe.ingredients}
+                    onChange={ (e) => setRecipe({...recipe, ingredients: e.target.value })}/>
+                </div>
+                <div>
+                    <label>Directions: </label>
+                    <textarea value={recipe.directions}
+                    onChange={ (e) => setRecipe({...recipe, directions: e.target.value })}/>
+                </div>
+                <div>
+                    <label>Description: </label>
+                    <textarea value={recipe.description}
+                    onChange={ (e) => setRecipe({...recipe, description: e.target.value })}/>
+                </div>
+                <div>
+                    <label>Select Image File: </label>
+                    <input type="file" value={recipe.image} src={recipe.image}
+                    onChange={ (e) => setRecipe({...recipe, image: e.target.value })}></input>
+                </div>
+                <button type='submit'>Add Recipe</button>
+            </form>
+        </div>
+    )
+}
+export default AddRecipe;
